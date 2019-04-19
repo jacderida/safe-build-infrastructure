c639961ae8b9b197d2ff89b4ebf066a3b599d700
Chris O'Neil <chris.oneil@gmail.com>
2019-04-19 16:19:30 +0100

    feat/build: multibranch pipeline for SCL

--- ansible/roles/jenkins-master/files/job_dsl_seed.groovy
+++ ansible/roles/jenkins-master/files/job_dsl_seed.groovy
@@ -1,3 +1,24 @@
+multibranchPipelineJob('pipeline-safe_client_libs') {
+    branchSources {
+        github {
+            checkoutCredentialsId('github_maidsafe_token_credentials')
+            scanCredentialsId('github_maidsafe_token_credentials')
+            repoOwner('maidsafe')
+            repository('safe_client_libs')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('scripts/Jenkinsfile')
+        }
+    }
+}
+
 multibranchPipelineJob('pipeline-jenkins_sample_lib') {
     branchSources {
         github {
